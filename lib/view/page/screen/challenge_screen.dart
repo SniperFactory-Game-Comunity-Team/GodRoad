750c1c8d64ac4b82ee6b3347496a57450db76d84
jieun27 <zzmfha27@naver.com>
2023-04-06 22:47:40 +0900

    메인페이지 데이터 삽입

---
+++ lib/view/page/screen/challenge_screen.dart
@@ -0,0 +1,113 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:godroad/controller/main_controller.dart';
+import 'package:godroad/model/challenge.dart';
+import 'package:godroad/util/keyword.dart';
+import 'package:godroad/view/widget/for_tile.dart';
+import 'package:godroad/view/widget/real_time_tile.dart';
+
+class ChallengeScreen extends GetView<MainController> {
+  const ChallengeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                '실시간 인기 챌린지',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('모두보기'),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 200,
+          child: FutureBuilder<RxList<QueryDocumentSnapshot<Challenge>>>(
+              future: controller.readChallenge(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData &&
+                    snapshot.connectionState == ConnectionState.done) {
+                  return Obx(() => ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        return RealTimeTile(
+                          challenge: snapshot.data![index].data(),
+                        );
+                      }));
+                }
+                return const SizedBox();
+              }),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '${controller.auth.userProfile!.nickname}님을 위한 챌린지',
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('모두보기'),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 200,
+          child: FutureBuilder<RxList<QueryDocumentSnapshot<Challenge>>>(
+              future: controller.readChallenge(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData &&
+                    snapshot.connectionState == ConnectionState.done) {
+                  return Obx(() => ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        return ForTile(
+                          challenge: snapshot.data![index].data(),
+                        );
+                      }));
+                }
+                return SizedBox();
+              }),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                '키워드로 모아보기',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Wrap(
+                children: Keyword.keywords
+                    .map((e) => GestureDetector(
+                          onTap: () {},
+                          child: Chip(
+                            label: Text(e),
+                          ),
+                        ))
+                    .toList(),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/view/widget/real_time_tile.dart
+++ lib/view/widget/real_time_tile.dart
@@ -10,24 +10,26 @@ class RealTimeTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 16.0, right: 16),
-      child: Card(
-        color: Colors.grey.shade300,
-        child: Row(
-          children: [
-            ClipRRect(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(8),
-                    bottomLeft: Radius.circular(8)),
-                child: Image(
-                    image: NetworkImage(challenge.mainPicture != ''
-                        ? challenge.mainPicture
-                        : 'https://picsum/photos/100/100'))),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: SizedBox(
-                width: 250,
+    return Card(
+      color: Colors.grey.shade300,
+      child: Row(
+        children: [
+          ClipRRect(
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(8), bottomLeft: Radius.circular(8)),
+              child: Image(
+                image: NetworkImage(challenge.mainPicture != ''
+                    ? challenge.mainPicture
+                    : 'https://picsum/photos/100/100'),
+                width: 100,
+                height: 100,
+              )),
+          Padding(
+            padding: const EdgeInsets.only(left: 3.0),
+            child: SizedBox(
+              width: 200,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -36,7 +38,7 @@ class RealTimeTile extends StatelessWidget {
                       children: [
                         Text(
                           challenge.title,
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                          style: const TextStyle(fontWeight: FontWeight.bold),
                         ),
                         Container(
                           width: 45,
@@ -44,7 +46,7 @@ class RealTimeTile extends StatelessWidget {
                           decoration: BoxDecoration(
                               color: Colors.grey,
                               borderRadius: BorderRadius.circular(16)),
-                          child: Text(
+                          child: const Text(
                             'D-15',
                             style: TextStyle(color: Colors.white),
                             textAlign: TextAlign.center,
@@ -52,29 +54,30 @@ class RealTimeTile extends StatelessWidget {
                         )
                       ],
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 5,
                     ),
                     Text(
                       challenge.content,
-                      style: TextStyle(fontSize: 12),
+                      style: const TextStyle(fontSize: 12),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 30,
+                    ),
+                    Wrap(
+                      children: challenge.keyword
+                          .map((e) => GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  e,
+                                  style: const TextStyle(fontSize: 12),
+                                ),
+                              ))
+                          .toList(),
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Wrap(
-                          children: challenge.keyword
-                              .map((e) => GestureDetector(
-                                    onTap: () {},
-                                    child: Chip(
-                                      label: Text(e),
-                                    ),
-                                  ))
-                              .toList(),
-                        ),
+                      children: const [
                         SizedBox(
                           width: 10,
                         ),
@@ -99,9 +102,9 @@ class RealTimeTile extends StatelessWidget {
                   ],
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
