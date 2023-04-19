21d4c04b36a158ea4a7359d6bc106acecd80b5c6
YHW505 <guddn505@naver.com>
2023-04-19 23:56:19 +0900

    종료된 챌린지 디자인 수정

--- lib/view/page/screen/incomplete_challenge_screen.dart
+++ lib/view/page/screen/incomplete_challenge_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:godroad/view/widget/my_page_challenge_list_tile_complete.dart';
 import 'package:godroad/view/widget/my_page_challenge_list_tile_incomplete.dart';
 
@@ -10,7 +11,7 @@ class InCompleteChallengeScreen extends StatelessWidget {
     return Column(
       children: [
         SizedBox(
-          height: 600,
+          height: Get.size.height * 0.8,
           child: ListView.separated(
               shrinkWrap: true,
               itemBuilder: (context, index) {
